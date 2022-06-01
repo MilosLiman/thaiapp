bec2ad7b1a3eea72b2e191ab60f4b64a853a91a9
Milos C <milos@milos.com>
2022-06-01 11:43:33 +0200

    Second commit

--- public/js/app.js
+++ public/js/app.js
@@ -191,29 +191,3 @@ function trainingPrice(){
 }
 
 trainingPrice();
-
-// var duration_t = new Array();
-// duration_t["week"] = 1;
-// duration_t["month"] = 2;
-
-// function duration(){
-
-//     var duration = 0;
-//     const selectedDuration = general.elements['duration'];
-
-//     duration = duration_t[selectedDuration.value];
-
-//     return duration;
-
-// }
-
-
-// function totalPrice(){
-    
-//      var totalPr = 0;
-
-//      totalPr = trainingPrice() * duration();
-
-//     price.innerHTML = totalPr;
-
-// }
